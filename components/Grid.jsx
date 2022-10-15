dd9088699199edd0cdfd19bb430b71d43c4d0249
Emil <MCAzurow@gmail.com>
2022-10-15 15:44:36 +0200

    Finished off Letzte Hilfe page

--- components/Grid.jsx
+++ components/Grid.jsx
@@ -7,61 +7,61 @@ export default function Grid() {
     return (
         <section className={styles.grid}>
             <div className={`${styles.Picture1} ${styles.Picture}`}>
-                <h2>test</h2>
-                <p>Lorem ipsum. Dolor sit amet. Lorem Ipsum dolor sit. Dolor amet sit Lorem ipsum.</p>
+                <h2>„Ciao, Kakao!“ </h2>
+                <p>Wir stehen den Schüler:innen und Pädagog:innen zur Seite und begleiten sie achtsam und fürsorglich durch die Zeit. Das Team Trost besteht aus Trauerbegleiter:innen, systemischen Coaches, pädagogischen Mitarbeiter:innen, Bestattungshelfer:innen und Ritualgestalter:innen.</p>
                 <Image
                     alt='Bild des Endlichkeits-Teams'
-                    src={'/images/start/text.jpg'}
+                    src={'/images/start/grid-1.jpg'}
                     layout='fill'
                     objectFit='cover'
                 />
             </div>
             <div className={`${styles.Picture2} ${styles.Picture}`}>
-                <h2>test</h2>
-                <p>Lorem ipsum. Dolor sit amet. Lorem Ipsum dolor sit. Dolor amet sit Lorem ipsum.</p>
+                <h2>„Wellenreiter“ </h2>
+                <p>Wir befinden uns in dieser Gruppe in einem geschützten Raum, in dem wir unsere Sorgen, Ängste, Trauer, Wut, Enttäuschung zeigen und aussprechen können Wir treffen uns an einem vereinbarten Ort, lernen uns kurz kennen und laufen dann los. Wer nicht mit einer Gruppe laufen, sich aber in der Natur bewegen und begleitet sein möchte, kann ebenfalls mit dem Team Trost einen Termin vereinbaren.</p>
                 <Image
                     alt='Bild des Endlichkeits-Teams'
-                    src={'/images/start/text.jpg'}
+                    src={'/images/start/grid-2.jpg'}
                     layout='fill'
                     objectFit='cover'
                 />
             </div>
             <div className={`${styles.Picture3} ${styles.Picture}`}>
-                <h2>test</h2>
-                <p>Lorem ipsum. Dolor sit amet. Lorem Ipsum dolor sit. Dolor amet sit Lorem ipsum.</p>
+                <h2>Weglaufen</h2>
+                <p>Natur und Bewegung - eine Kraftquelle. Sie entspannt, beruhigt, regt das Immunsystem an, ist Balsam für die Seele. Mit jemandem reden, sich austauschen, schweigen, aber auch lachen, die Natur riechen, Tiere und Pflanzen beobachten, Wind und Wetter erleben… All das beruhigt die Gedanken und fokussiert uns auf den Augenblick. </p>
                 <Image
                     alt='Bild des Endlichkeits-Teams'
-                    src={'/images/start/text.jpg'}
+                    src={'/images/start/grid-3.jpg'}
                     layout='fill'
                     objectFit='cover'
                 />
             </div>
             <div className={`${styles.Picture4} ${styles.Picture}`}>
-                <h2>test</h2>
-                <p>Lorem ipsum. Dolor sit amet. Lorem Ipsum dolor sit. Dolor amet sit Lorem ipsum.</p>
+                <h2>Blablabla…</h2>
+                <p>Du bist genervt, weil alle ständig mit Dir über Deine Gefühle sprechen wollen, Du aber nicht? Du willst ganz laut f*** schreien, den ganzen Tag, weil jemand gestorben ist und es keinen interessiert, wie es Dir geht oder…? Dann schreib uns eine Mail blabla[at]endlichkeit.jetzt oder Mobil Message an 0151 25288191</p>
                 <Image
                     alt='Bild des Endlichkeits-Teams'
-                    src={'/images/start/text.jpg'}
+                    src={'/images/start/grid-4.jpg'}
                     layout='fill'
                     objectFit='cover'
                 />
             </div>
             <div className={`${styles.Picture5} ${styles.Picture}`}>
-                <h2>test</h2>
-                <p>Lorem ipsum. Dolor sit amet. Lorem Ipsum dolor sit. Dolor amet sit Lorem ipsum.</p>
+                <h2>Seelenmassage </h2>
+                <p>Der Körper speichert alle Emotionen und alle Traumata. Für die Seele ist im Alltag meistens keine Zeit und sie kommt nicht „hinterher“.  Körper, Geist und Seele sollen in Einklang kommen und so das Erlebte auf eine gesunde Art und Weise umsetzen. Durch das Gespräch während der Behandlung können Sorgen und Gedanken geteilt und Lösungen gesucht werden.</p>
                 <Image
                     alt='Bild des Endlichkeits-Teams'
-                    src={'/images/start/text.jpg'}
+                    src={'/images/start/grid-5.jpg'}
                     layout='fill'
                     objectFit='cover'
                 />
             </div>
             <div className={`${styles.Picture6} ${styles.Picture}`}>
-                <h2>test</h2>
-                <p>Lorem ipsum. Dolor sit amet. Lorem Ipsum dolor sit. Dolor amet sit Lorem ipsum.</p>
+                <h2>Weglaufen</h2>
+                <p>Natur und Bewegung - eine Kraftquelle. Sie entspannt, beruhigt, regt das Immunsystem an, ist Balsam für die Seele. Mit jemandem reden, sich austauschen, schweigen, aber auch lachen, die Natur riechen, Tiere und Pflanzen beobachten, Wind und Wetter erleben… All das beruhigt die Gedanken und fokussiert uns auf den Augenblick.</p>
                 <Image
                     alt='Bild des Endlichkeits-Teams'
-                    src={'/images/start/text.jpg'}
+                    src={'/images/start/grid-6.jpg'}
                     layout='fill'
                     objectFit='cover'
                 />
--- pages/letzte-hilfe.jsx
+++ pages/letzte-hilfe.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/LetzteHilfe.module.scss'
@@ -8,6 +8,9 @@ import Button from '../components/Button'
 import Carousel from '../components/Carousel'
 
 export default function LetzteHilfe() {
+
+    const [selectedOption, setOption] = useState(0);
+
   return (
     <PageContainer>
         <Head>
@@ -19,52 +22,125 @@ export default function LetzteHilfe() {
 
         <h2>Letzte Hilfe - Am Ende wissen, wie es geht </h2>
 
-        <section className={styles.description}>
-            <div>
-                <p>Der Abschied vom Leben ist der schwerste, den die Lebensreise für einen Menschen bereithält. Deshalb braucht es, wie auf allen schweren Wegen, jemanden, der die Hand reicht.</p>
-                <p>Wir möchten Grundwissen vermitteln und ermutigen, sich Sterbenden zuzuwenden. Denn Zuwendung ist das, was wir alle am Ende des Lebens am meisten brauchen.</p>
-                <p>Das Lebensende und Sterben unserer Angehörigen, Freunde und Nachbarn macht uns oft hilflos, deshalb bieten wir einen Kurs zur „Letzten Hilfe“ an. Unser Ziel ist es, Basiswissen, Orientierungen und einfache Handgriffe weiterzugeben. </p>
-                <p>Im Kurs sprechen wir über die Normalität des Sterbens als Teil des Lebens, natürlich werden auch Patientenverfügung und Vorsorgevollmacht kurz angefügt. Wir thematisieren mögliche Leiden als Teil des Sterbeprozesses und wie wir lindern können. Zudem gehen wir darauf ein, wie man mit den schwereren, aber auch den leichteren Stunden umgeht. Wir überlegen abschließend gemeinsam, wie man Abschied nehmen kann und besprechen unsere Möglichkeiten und Grenzen.</p>
-            </div>
-            
-            <aside className={styles.description_aside}>
-                <div className={styles.image}>
-                    <Image
-                    alt='Post-Its'
-                    src={"/images/letzte-hilfe/text-1.JPG"}
-                    layout="fill"
-                    objectFit='cover'
-                    quality={100}
-                    />
+        <nav onChange={e => setOption(e.target.value)}>
+            <label className={styles.nav_button}>
+                <input type="radio" name="age-select" value={0} defaultChecked />
+                <h3>für Erwachsene</h3>
+            </label>
+            <label className={styles.nav_button}>
+                <input type="radio" name="age-select" value={1} />
+                <h3>für Kinder und Jugendliche</h3>
+            </label>
+        </nav>
+
+        
+        {/*Erwachsene */}
+        <div className={selectedOption == 1 ? styles.hidden : styles.shown}>
+            <section className={styles.description}>
+                <div className={styles.description_text}>
+                    <p>Der Abschied vom Leben ist der schwerste, den die Lebensreise für einen Menschen bereithält. Deshalb braucht es, wie auf allen schweren Wegen, jemanden, der die Hand reicht.</p>
+                    <p>Wir möchten Grundwissen vermitteln und ermutigen, sich Sterbenden zuzuwenden. Denn Zuwendung ist das, was wir alle am Ende des Lebens am meisten brauchen.</p>
+                    <p>Das Lebensende und Sterben unserer Angehörigen, Freunde und Nachbarn macht uns oft hilflos, deshalb bieten wir einen Kurs zur „Letzten Hilfe“ an. Unser Ziel ist es, Basiswissen, Orientierungen und einfache Handgriffe weiterzugeben. </p>
+                    <p>Im Kurs sprechen wir über die Normalität des Sterbens als Teil des Lebens, natürlich werden auch Patientenverfügung und Vorsorgevollmacht kurz angefügt. Wir thematisieren mögliche Leiden als Teil des Sterbeprozesses und wie wir lindern können. Zudem gehen wir darauf ein, wie man mit den schwereren, aber auch den leichteren Stunden umgeht. Wir überlegen abschließend gemeinsam, wie man Abschied nehmen kann und besprechen unsere Möglichkeiten und Grenzen.</p>
                 </div>
-                <div className={styles.image}>
-                    <Image
-                        alt='Textmarker'
-                        src={"/images/letzte-hilfe/text-2.JPG"}
+            
+                <aside className={styles.description_aside}>
+                    <div className={styles.image}>
+                        <Image
+                        alt='Post-Its'
+                        src={"/images/letzte-hilfe/text-1.JPG"}
                         layout="fill"
                         objectFit='cover'
                         quality={100}
-                        />                    
-                </div>
-            </aside>
-        </section>
-
-        <section className={styles.contents}>
+                        />
+                    </div>
+                    <div className={styles.image}>
+                        <Image
+                            alt='Textmarker'
+                            src={"/images/letzte-hilfe/text-2.JPG"}
+                            layout="fill"
+                            objectFit='cover'
+                            quality={100}
+                            />
+                    </div>
+                </aside>
+            </section>
+            <section className={styles.contents}>
             
-            <Carousel/>
+                <Carousel/>
+                <div className={styles.contents_text}>
+                    <h2>Kursinhalte</h2>
+                    <p>Wie auch bei der Ersten Hilfe soll die Letzte Hilfe Wissen zur humanen Hilfe und Mitmenschlichkeit vermitteln. Die vier Unterrichtsstunden stehen jeweils für eines der vier Module:</p>
+                    <ul className={styles.contents_list}>
+                        <li>Sterben ist ein Teil des Lebens</li>
+                        <li>Vorsorgen und Entscheiden</li>
+                        <li>Leiden lindern</li>
+                        <li>Abschied nehmen</li>
+                    </ul>
+                    <Button text={"Buchen"} />
+                    </div>
+            </section>
+        </div>
 
-            <div>
-                <h2>Kursinhalte</h2>
-                <p>Wie auch bei der Ersten Hilfe soll die Letzte Hilfe Wissen zur humanen Hilfe und Mitmenschlichkeit vermitteln. Die vier Unterrichtsstunden stehen jeweils für eines der vier Module:</p>
-                <ul className={styles.contents_list}>
-                    <li>Sterben ist ein Teil des Lebens</li>
-                    <li>Vorsorgen und Entscheiden</li>
-                    <li>Leiden lindern</li>
-                    <li>Abschied nehmen</li>
-                </ul>
-                <Button text={"Buchen"} />
+        {/*Kinder */}
+        <div className={selectedOption == 0 ? styles.hidden : styles.shown}>
+            <section className={styles.description}>
+                <div className={styles.description_text}>
+                    <p>Wir bieten einen Basis-Kurs zur Letzten Hilfe für Kinder und Jugendliche von 8 - 16 Jahren an, in dem Kinder und Jugendliche lernen, was sie für ihre Mitmenschen am Ende des Lebens tun können und wie sie andere Menschen in Trauer verstehen lernen. </p>
+                    <p>Wissen um Letzte Hilfe und Umsorgung von schwerkranken und sterbenden Menschen muss auch für Kinder und Jugendliche, zum Allgemeinwissen werden. Häufig fällt es uns besonders schwer mit Personen umzugehen, die hilflos sind, weil jemand lebensbedrohlich erkrankt ist. So sollten betroffene junge Menschen gut vorbereitet sein ihre Mitmenschen in so einem wichtigen Lebensabschnitt begleiten zu können.</p>
+                    <p>Wir möchten Grundwissen an die Hand geben und ermutigen, sich Sterbenden und deren Angehörigen zuzuwenden. Denn Zuwendung ist das, was wir alle am Ende des Lebens am meisten brauchen.</p>
+                    <ul>
+                        <li>
+                            <p>richtet sich an Kinder und Jugendliche, die sich über die Themen rund um das Sterben, Tod und Palliativversorgung informieren wollen</p>
+                        </li>
+                        <li>
+                            <p>ist ein wunderbares Bildungskonzept für jegliche Bildungseinrichtungen und kann in der Schule, im Konfirmandenunterricht, in Vereinen oder zu anderen Anlässen gegeben werden.</p>
+                        </li>
+                        <li>
+                            <p>schafft Grundlagen und hilft mit, die allgemeine ambulante Palliativversorgung zu verbessern</p>
+                        </li>
+                    </ul>
                 </div>
-        </section>
+            
+                <aside className={styles.description_aside}>
+                    <div className={styles.image}>
+                        <Image
+                        alt='Post-Its'
+                        src={"/images/letzte-hilfe/text-1.JPG"}
+                        layout="fill"
+                        objectFit='cover'
+                        quality={100}
+                        />
+                    </div>
+                    <div className={styles.image}>
+                        <Image
+                            alt='Textmarker'
+                            src={"/images/letzte-hilfe/text-2.JPG"}
+                            layout="fill"
+                            objectFit='cover'
+                            quality={100}
+                            />
+                    </div>
+                </aside>
+            </section>
+            <section className={styles.contents}>
+            
+                <Carousel/>
+                <div className={styles.contents_text}>
+                    <h2>Kursinhalte</h2>
+                    <p>Im Kurs sprechen wir nicht nur über die Normalität des Sterbens als Teil des Lebens, sondern erarbeiten viele Grundsätze interaktiv und spielerisch. Natürlich werden auch Themen wie Patientenverfügung und Vorsorgevollmacht kurz besprochen. Wir thematisieren mögliche Beschwerden die Teil des Sterbeprozesses sein können und wie wir bei der Linderung helfen können. Wir überlegen abschließend gemeinsam, wie man Abschied nehmen kann und besprechen unsere Möglichkeiten und Grenzen, ebenso Rituale, die eine schöne Erinnerung aufbauen.</p>
+                    <ul className={styles.contents_list}>
+                        <li>Sterben ist ein Teil des Lebens</li>
+                        <li>Vorsorgen und Entscheiden</li>
+                        <li>Leiden lindern</li>
+                        <li>Abschied nehmen</li>
+                    </ul>
+                    <Button text={"Buchen"} />
+                    </div>
+            </section>
+        </div>
+
+        
     </PageContainer>
   )
 }
