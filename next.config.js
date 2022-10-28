1e5195b2b1a546bcef00a207f031ffaf556d6103
Emil <MCAzurow@gmail.com>
2022-10-28 14:53:28 +0200

    opt

--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // images: 
-  // {
-  //   unoptimized: true,
-  // }
+  images: 
+  {
+    unoptimized: true,
+  }
 }
 
 module.exports = nextConfig
